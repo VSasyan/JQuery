c39edf2d0e01fff6bda3e7631dbc2b7cd4fdb8a9
Valentin Sasyan <VSasyan@users.noreply.github.com>
2016-12-09 11:43:30 +0100

    Update nextInput.jquery.js
    Backspace does not bug anymore, and the user can use tab and other key without issue.

--- nextInput/nextInput.jquery.js
+++ nextInput/nextInput.jquery.js
@@ -14,18 +14,34 @@
 					target = '[data-next-input="' + data + '"]' + target;
 				}
 
-				// Need to switch to next/previous ?
+				// Need to switch to next ?
+				console.log(event.keyCode)
+				if (length == this.maxLength && event.keyCode > 47) {
+
+					var $next = $(this).next(target);
+					$next.focus();
+
+				}
+
+			});
+
+			$(this).keydown(function (event) {
+
+				var target = '.nextInput';
+				var data = $(this).data('nextInput');
+				var length = this.value.length;
+
+				// How to target next./previous ?				
+				if (data != undefined) {
+					target = '[data-next-input="' + data + '"]' + target;
+				}
+
+				// Need to switch to previous ?
 				if (event.keyCode == 8 && length == 0) {
 
 					var $prev = $(this).prev(target);
 					var val = $prev.val();
-					$prev.val(val.substr(0, val.length - 1));
 					$prev.focus();
-
-				} else if (length == this.maxLength) {
-
-					var $next = $(this).next(target);
-					$next.focus();
 
 				}
 
